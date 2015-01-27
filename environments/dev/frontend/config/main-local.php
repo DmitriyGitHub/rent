4e350f4baa52f90d692484a6ce6e88c57b74abc1
dmitriy <dmitriy.raschepkin@gmail.com>
2015-01-27 22:32:01 +0200

    Config changes for frontend.

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -5,8 +5,11 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 's9enwG2XXFqVEpLIJLUM',
+            'baseUrl' => '',
         ],
         'urlManager' => [
+            'baseUrl' => '',
+            'class' => 'yii\web\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
         ],
