f02155e6bf1abca0019be1e6c3284d88c6e3c128
Dmitriy Raschepkin <dmitriy.raschepkin@gmail.com>
2015-01-27 09:23:38 +0200

    Backend added as a subdirectory instead of subdomain. Config and Vagrant changes

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -5,8 +5,11 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'K19pq2xMDZsxOodzgK4G',
+            'baseUrl' => '/backend',
         ],
         'urlManager' => [
+            'baseUrl' => '/backend',
+            'class' => 'yii\web\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
         ],
