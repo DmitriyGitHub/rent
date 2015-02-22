4af22209baee9051917ce07f815902949ef87cba
dmitriy <dmitriy.raschepkin@gmail.com>
2015-02-22 22:55:59 +0200

    Main backend layout fixes

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -25,18 +25,18 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => Yii::t('app', 'Rent'),
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
                 ],
             ]);
             $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
+                ['label' => Yii::t('yii', 'Home'), 'url' => ['/site/index']],
             ];
             if (Yii::$app->user->isGuest) {
                 $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {    
+            } else if(\common\models\User::hasBackendAccess()) {    
                 $menuItems[] = ['label' => Yii::t('app', 'Organisations'), 'url' => ['/organisations']];
                 $menuItems[] = ['label' => Yii::t('app', 'Payments'),
                     'items' => [
@@ -89,7 +89,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?= Yii::t('app', 'Rent') . ' ' . date('Y') ?></p>
         <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
